6071083bff3d7bb8f3bc86d1a2eb91403a1973a2
David Barratt <david.barratt@golfchannel.com>
2015-08-18 12:47:09 -0400

    DRUPAL-2595 Make the library class immutable

--- src/Library/Library.php
+++ src/Library/Library.php
@@ -6,10 +6,12 @@
 
 namespace Drupal\block_render\Library;
 
+use Drupal\block_render\Immutable;
+
 /**
  * Single Library.
  */
-class Library implements LibraryInterface {
+final class Library extends Immutable implements LibraryInterface {
 
   /**
    * Name of the library.
@@ -39,40 +41,10 @@ class Library implements LibraryInterface {
   }
 
   /**
-   * Sets the Name.
-   *
-   * @param string $name
-   *   Name of the library.
-   *
-   * @return \Drupal\block_render\Library\Libarary
-   *   Current library object.
-   */
-  public function setName($name) {
-    $this->name = $name;
-
-    return $this;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function getName() {
     return $this->name;
-  }
-
-  /**
-   * Sets the Name.
-   *
-   * @param string $version
-   *   Name of the version.
-   *
-   * @return \Drupal\block_render\Library\Libarary
-   *   Current library object.
-   */
-  public function setVersion($version) {
-    $this->version = $version;
-
-    return $this;
   }
 
   /**
